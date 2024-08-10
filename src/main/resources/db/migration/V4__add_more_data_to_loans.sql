fbf82a15230ade4d25d94327ee460ae6a0c1ae9e
AZZohaib <zohaib.ali@kainos.com>
2024-08-10 12:10:58 +0100

    adding more data to loans

---
+++ src/main/resources/db/migration/V4__add_more_data_to_loans.sql
@@ -0,0 +1,5 @@
+INSERT INTO Loans (member_id, book_id, loan_date, return_date) VALUES
+(2, 6, '2024-06-20', NULL),
+(3, 7, '2024-06-12', NULL),
+(1, 8, '2024-08-01', NULL),
+(2, 8, '2024-08-03', NULL);
